17516884a6245d2934d9d511ad8a4039ef54bf7e
Tony Yu <tony.yu@rice.edu>
2025-04-27 04:07:50 +0000

    slider

--- static/js/index.js
+++ static/js/index.js
@@ -1,78 +1,64 @@
+// ─── INTERPOLATION SLIDER SCRIPT ───
 window.HELP_IMPROVE_VIDEOJS = false;
 
-var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 593;
+// default folder & frame count (will be overridden on click)
+var INTERP_BASE       = "./static/interpolation/stacked";
+var NUM_INTERP_FRAMES = 197;
 
 var interp_images = [];
+
 function preloadInterpolationImages() {
+  interp_images = [];
   for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(5, '0') + '.png';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
+    var img = new Image();
+    img.src = INTERP_BASE + "/" + String(i).padStart(5, "0") + ".png";
+    interp_images.push(img);
   }
 }
 
-function setInterpolationImage(i) {
-  var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
+function setInterpolationImage(idx) {
+  var img = interp_images[idx];
+  img.ondragstart   = () => false;
+  img.oncontextmenu = () => false;
+  $("#interpolation-image-wrapper")
+    .empty()
+    .append(img);
 }
 
-
 $(document).ready(function() {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+  // 1) initial load
+  preloadInterpolationImages();
+  setInterpolationImage(0);
+  $("#interpolation-slider")
+    .prop("max", NUM_INTERP_FRAMES - 1);
 
-    });
+  // 2) slider moves
+  $("#interpolation-slider").on("input", function() {
+    setInterpolationImage(this.value);
+  });
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 3,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
-    }
+  // 3) env-map button clicks
+  $(".env-button").on("click", function() {
+    // grab new folder & frame count
+    INTERP_BASE       = $(this).data("base");
+    NUM_INTERP_FRAMES = parseInt($(this).data("frames"), 10);
 
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
-
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
-
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
+    // reload frames
     preloadInterpolationImages();
 
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
+    // reset slider range & thumb
+    $("#interpolation-slider")
+      .prop("max", NUM_INTERP_FRAMES - 1)
+      .val(0);
+
+    // draw first frame
     setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
 
-    bulmaSlider.attach();
+    // optional: swap the source-envmap thumbnail to match
+    $("#source-envmap")
+      .attr("src", $(this).attr("src").replace("_thumb", ""));
+  });
 
-})
+  // reattach bulmaSlider if you need it elsewhere
+  if (window.bulmaSlider) bulmaSlider.attach();
+});
